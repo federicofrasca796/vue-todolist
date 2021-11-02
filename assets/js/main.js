e742a2a492f43f1d02eda9fc77d9a5caed163a5b
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-02 12:08:49 +0100

    add html struct | add vue instance

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        tasks: [
+            "Completare l'esercizio di oggi",
+            "Completare il bonus.",
+            "Contemplare il proprio amore per VueJS",
+        ],
+
+        logo: 'https://www.boolean.careers/images/misc/logo.png',
+
+    },
+
+    methods:{
+        
+    },
+})
