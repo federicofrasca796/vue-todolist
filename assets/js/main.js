f671c3431dbde2d54dff7f16d5d2797f91baac92
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-02 12:49:25 +0100

    add addTask function

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,15 +6,25 @@ const app = new Vue({
             "Completare il bonus.",
             "Contemplare il proprio amore per VueJS",
         ],
-
+        newTask: '',
         logo: 'https://www.boolean.careers/images/misc/logo.png',
 
+        error: false,
     },
 
     methods:{
         deleteTask(index){
             this.tasks.splice(index, 1)
             console.log(this.tasks);
+        },
+
+        addTask(){
+            if (this.newTask.length > 2){
+                this.tasks.push(this.newTask);
+                this.newTask = '';
+            } else {
+                this.error = true;
+            }
         },
     },
 })
